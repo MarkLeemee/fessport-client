e7bc6fd07af4c5f2892bf571ba97a5b3beaf0c44
markleemee <markleemee@gmail.com>
2021-01-30 17:15:38 +0900

    setting

--- src/pages/ResellPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import ResellContainer from '../containers/ResellContainer';
-
-const ResellPage = (): JSX.Element => {
-  return <>{/* <ResellContainer /> */}</>;
-};
-
-export default ResellPage;
